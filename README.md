e16212958fb4a0bbe384e4b26d1b13d710cec35a
timolp <timmothy86264020@gmail.com>
2022-10-09 04:04:29 +0200

    All working downloader

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Youtube-Playlist-MP3-Downloader
+
+A simple youtube music playlist downloader, to always have your favourite music playlist in your pocket when offline 📵
+### How to use ?
+
+Run main.py
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ playlist_URL = 'https://www.youtube.com/playlist?list=FLNPzWyOogzgJktfz1Uw76hQ'
 full_playlist = Playlist(playlist_URL)
 output_path = 'music/'
 
-
 # Fix the video numbers
 validation = 'n'
 while validation != 'y':
-    n = int(input('Enter the number of youtube videos to download: '))
+    n = int(input('\nEnter the number of youtube videos to download: '))
+    print()
     playlist = full_playlist[:n]
     for URL in playlist:
         vid = YouTube(URL)
@@ -20,19 +20,29 @@ while validation != 'y':
         print('Length:', datetime.timedelta(seconds=vid.length), 's')
         print('URL:', URL)
         print()
-    validation = input('Good ? (y/n)')
-
+    validation = input('Good ? (y/n)      ')
 
 # Select the correct streams (Adaptive/DASH, audio, .mp4)
-print("Downloading ...")
+print('\nDownloading ...\n')
 for URL in tqdm(playlist):
+
+    # Fetch the streams
     vid = YouTube(URL)
     title = vid.title
     audios_dash_mp4 = vid.streams.filter(adaptive=True,
                                          only_audio=True,
                                          file_extension='mp4')
-    # Download the stream
-    best_audio = audios_dash_mp4[-1]
-    best_audio.download(output_path)
 
-print('Download completed !')
+    # Download a stream to .mp4
+    best_audio = audios_dash_mp4[-1]
+    best_audio.download(output_path=output_path,
+                        filename=f'{title}.mp4',
+                        max_retries=3)
+
+    # Convert .mp4 to .mp3
+    mp4_file = f'{title}.mp4'
+    mp3_file = f'{title}.mp3'
+    ff.ffmpeg_extract_audio(output_path+mp4_file, output_path+mp3_file)
+    os.remove(output_path+mp4_file)
+
+print('\nDownload completed ! ✅')
