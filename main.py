65130ceb8e0a8f48acb497107c34671ece960574
timolp <timmothy86264020@gmail.com>
2022-10-09 02:10:14 +0200

    working ffmpeg to implement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pytube import YouTube, Playlist
+from moviepy.editor import *
 from tqdm import tqdm
 import datetime
+import os
 
 playlist_URL = 'https://www.youtube.com/playlist?list=FLNPzWyOogzgJktfz1Uw76hQ'
 full_playlist = Playlist(playlist_URL)
@@ -21,12 +23,31 @@ while validation != 'y':
     validation = input('Good ? (y/n)')
 
 
-# Select the correct streams
+# Select the correct streams (Adaptive/DASH, audio, .mp4)
+print("Downloading ...")
 for URL in tqdm(playlist):
     vid = YouTube(URL)
-    audios_dash_mp4 = vid.streams.filter(only_audio=True,
-                                         adaptive=True,
+    title = vid.title
+    audios_dash_mp4 = vid.streams.filter(adaptive=True,
+                                         only_audio=True,
                                          file_extension='mp4')
+    # Download the stream
+    best_audio = audios_dash_mp4[-1]
+    best_audio.download(output_path)
 
-    for i in audios_dash_mp4:
-        print(i)
+print('Download completed !')
+
+mp4_file = 'Timecop1983 - On the Run 2' + '.mp4'
+mp3_file = 'Timecop1983 - On the Run 2' + '.mp3'
+
+videoclip = VideoFileClip(output_path + mp4_file)
+audioclip = videoclip.audio
+audioclip.write_audiofile(output_path + mp3_file)
+audioclip.close()
+videoclip.close()
+os.remove(output_path + mp4_file)
+
+
+# base, ext = os.path.splitext(out_file)
+# new_file = base + '.mp3'
+# os.rename(out_file, new_file)
