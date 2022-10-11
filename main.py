c96e443dbb6f5668de26fb6403f5155f65d63d86
timolp <timmothy86264020@gmail.com>
2022-10-11 02:43:19 +0200

    small changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ output_path = 'music/'
 
 # Set the download playlist
 validation = 'n'
-while validation != 'y' and validation != 'Y':
+while validation not in ['y', 'Y']:
     n_music = int(input('\nEnter the number of youtube musics to download: '))
     print('\n\n\n')
     playlist = full_playlist[:n_music]
@@ -27,7 +27,7 @@ while validation != 'y' and validation != 'Y':
 
         # If a music already exists, add its name to a list
         if os.path.exists(output_path+title+'.mp3'):
-            existing_files.append(title+'.mp3')
+            existing_files.append(f'{title}.mp3')
 
     # Validate if we keep this playlist, of if we want to set a new playlist
     validation = input('Good to download ? (y/n)      ')
@@ -38,11 +38,7 @@ if len(existing_files) > 0:
     print(f'⭐️ {len(existing_files)} musics already downloaded over {n_music} in the download list.')
     print('⭐️ These are not going to be downloaded again :')
     for i, music in enumerate(existing_files):
-        print(f'{i+1} - {music}')
-
-
-
-
+        print(f'{i+1}: {music}')
 
 # Select the correct streams (Adaptive/DASH, audio, .mp4)
 # and download the file
@@ -60,13 +56,13 @@ for URL in tqdm(playlist):
     # If the file does not already exist, download it
     if not os.path.exists(output_path+mp3_file):
         audios_dash_mp4 = mus.streams.filter(adaptive=True,
-                                            only_audio=True,
-                                            file_extension='mp4')
+                                             only_audio=True,
+                                             file_extension='mp4')
 
         # Download a stream to .mp4
-        best_audio = audios_dash_mp4[-1]
+        best_audio = audios_dash_mp4[-1]  # The last stream has the best quality
         best_audio.download(output_path=output_path,
-                            filename=f'.{title}.mp4',
+                            filename=mp4_file,
                             max_retries=3)
 
         # Convert .mp4 to .mp3
@@ -80,9 +76,7 @@ for URL in tqdm(playlist):
 
 print('\n\n\n')
 if n_music-len(bug_list) == n_music:
-    print(f'Required music downloaded ! ✅')
-else:    
+    print('Required music downloaded ! ✅')
+else:
     print('Bug detected \n')
     print(f'Downloaded {n_music-len(bug_list)} musics over {n_music} ! 🤔')
-
-
